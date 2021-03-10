e9ecd003a168c7a4d9a4b931d12c1a00054ce806
Artur MiRgaleev <easy0@inbox.ru>
2021-03-10 20:02:51 +0500

    new

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int current = 100;
+        int addition = 1400;
+        int bonus;
+
+        if (addition > 1000) {
+            bonus = addition / 100;
+
+        } else {
+            bonus = 0;
+        }
+
+
+        System.out.println(bonus);
+
+    }
+}
